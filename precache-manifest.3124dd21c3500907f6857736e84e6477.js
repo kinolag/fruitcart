215c148429146bddbc9c9e5d4ea4b5a7d1c8bddd
Nicola <ngrassini@yahoo.it>
2019-08-02 23:01:27 +0100

    Updates

---
+++ precache-manifest.3124dd21c3500907f6857736e84e6477.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da8951a9193048811c65cf4e514f235e",
+    "url": "/app/fruitcart/index.html"
+  },
+  {
+    "revision": "be22be677538e78001a6",
+    "url": "/app/fruitcart/static/css/main.0a690ca1.chunk.css"
+  },
+  {
+    "revision": "e8d1d6047b9c71644210",
+    "url": "/app/fruitcart/static/js/2.4772b873.chunk.js"
+  },
+  {
+    "revision": "be22be677538e78001a6",
+    "url": "/app/fruitcart/static/js/main.17815783.chunk.js"
+  },
+  {
+    "revision": "4f3f032e557b265e4ced",
+    "url": "/app/fruitcart/static/js/runtime~main.0c5c752d.js"
+  },
+  {
+    "revision": "09b866313e97d797c163eff52ef3c042",
+    "url": "/app/fruitcart/static/media/apples_oranges_1.09b86631.png"
+  }
+]);
